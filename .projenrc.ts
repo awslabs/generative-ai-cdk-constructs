6664e886b8970fcaef6b04b8539769d328aa1164
Alain Krok <alkrok@amazon.com>
2023-10-03 20:02:00 -0500

    add stale and monthly metrics workflows

--- .projenrc.ts
+++ .projenrc.ts
@@ -11,7 +11,7 @@
  *  and limitations under the License.
  */
 import { awscdk } from 'projen';
-import { buildMeritBadgerWorkflow } from './projenrc/github-workflows';
+import { buildMeritBadgerWorkflow, buildMonthlyIssuesMetricsWorkflow } from './projenrc/github-workflows';
 
 // Constants
 const GITHUB_USER = 'aws-samples';
@@ -62,10 +62,12 @@ const project = new awscdk.AwsCdkConstructLibrary({
   gitignore: ['*.DS_STORE'],
   stability: 'experimental',
   sampleCode: false,
+  stale: true
 });
 
 // Add some useful github workflows
 buildMeritBadgerWorkflow(project);
+buildMonthlyIssuesMetricsWorkflow(project);
 
 // Add License header automatically
 project.eslint?.addPlugins('header');
--- projenrc/github-workflows.ts
+++ projenrc/github-workflows.ts
@@ -43,6 +43,102 @@ export function buildMeritBadgerWorkflow(project: AwsCdkConstructLibrary)
         }); 
         workflow.addJobs({ 'call-action': applymeritbadge }); 
     }
-    }
-      
+    }   
 }
+
+/**
+ * https://github.com/aws/aws-cdk/blob/main/.github/workflows/repo-metrics.yml 
+ * GitHub action that runs monthly to report on metrics for issues and PRs created last month.
+ * @param project 
+ */
+export function buildMonthlyIssuesMetricsWorkflow(project: AwsCdkConstructLibrary)
+{
+  const buildjob: Job = {
+    permissions: { 
+      issues: JobPermission.WRITE,
+      pullRequests: JobPermission.READ, 
+    },
+    if: 'github.repository == aws-samples/emerging-tech-cdk-constructs',
+    runsOn: ["ubuntu-latest"], 
+    name: 'metrics',
+    steps:[
+      {
+        name:'Get dates for last month', 
+        run: [
+          '# Calculate the first day of the previous month',
+          'first_day=$(date -d "last month" +%Y-%m-01)',
+          '# Calculate the last day of the previous month',
+          'last_day=$(date -d $first_day +1 month -1 day +%Y-%m-%d)',
+          '# Set an environment variable with the date range',
+          'echo "$first_day..$last_day"',
+          'echo "last_month=$first_day..$last_day" >> "$GITHUB_ENV"',
+        ].join("\n"),
+      },
+      {
+        name: 'Report on issues',
+        uses: 'github/issue-metrics@v2',
+        env:
+        {
+          GH_TOKEN: "${{ secrets.PROJEN_GITHUB_TOKEN }}",
+          SEARCH_QUERY: 'repo:aws-samples/emerging-tech-cdk-constructs is:issue created:${{ env.last_month }} -reason:"not planned"'
+        }
+      },
+      {
+        name: 'Create report for issues',
+        uses: 'peter-evans/create-issue-from-file@v4',
+        with:
+        {
+          title: 'Monthly issue metrics report',
+          token: "${{ secrets.PROJEN_GITHUB_TOKEN }}",
+          'content-filepath': './issue_metrics.md',
+          assignees: 'krokoko'
+        }
+      },
+      {
+        name: 'Report on PRs',
+        uses: 'github/issue-metrics@v2',
+        env:
+        {
+          GH_TOKEN: "${{ secrets.PROJEN_GITHUB_TOKEN }}",
+          SEARCH_QUERY: 'repo:aws-samples/emerging-tech-cdk-constructs is:pr created:${{ env.last_month }} -is:draft'
+        }
+      },
+      {
+        name: 'Create report for PRs',
+        uses: 'peter-evans/create-issue-from-file@v4',
+        with:
+        {
+          title: 'Monthly issue metrics report',
+          token: "${{ secrets.PROJEN_GITHUB_TOKEN }}",
+          'content-filepath': './issue_metrics.md',
+          assignees: 'krokoko'
+        }
+      }
+    ]
+   }
+
+  if (project.github)
+    {
+    const workflow = project.github.addWorkflow('monthly-repo-metrics');
+    if (workflow)
+    {
+        workflow.on({ 
+          workflowDispatch: { 
+            schedule: {
+              cron: "0 2 1 * *"
+            } 
+        }, 
+        });
+        workflow.addJobs({ 'build': buildjob }); 
+    }
+    }  
+}
+
+/*
+` |
+        
+        
+
+        
+        `
+*/
