4aeaaac65cfe3af3ed1e449c38832fa67b3f1c4b
Alain Krok <alkrok@amazon.com>
2023-10-17 11:25:44 -0500

    add license header to all python scripts

--- lambda/question_answering/src/qa_agent/chain.py
+++ lambda/question_answering/src/qa_agent/chain.py
@@ -5,6 +5,18 @@ from .helper import load_vector_db_opensearch, send_job_status
 from .s3inmemoryloader import S3FileLoaderInMemory
 
 # all
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+# with the License. A copy of the License is located at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+#
 from langchain import PromptTemplate
 from llms import get_llm, get_max_tokens
 from langchain import LLMChain
