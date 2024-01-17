b7a2bb7498c720f100f683f746969f5e1405054e
Jeff Strunk <strunkjd@amazon.com>
2024-01-17 11:37:53 -0300

    feat(aoss-vectors): constructs and index custom resource

---
+++ src/common/helpers/aoss-vector.ts
@@ -0,0 +1,339 @@
+/**
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+import * as path from 'path';
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as oss from 'aws-cdk-lib/aws-opensearchserverless';
+import { Construct } from 'constructs';
+import { buildCustomResourceProvider } from './custom-resource-provider-helper';
+import { generatePhysicalName } from './utils';
+
+/**
+ * Deploys and OpenSearch Collection to be used as a vector store.
+ *
+ * It includes all policies.
+ */
+export class OpenSearchVectorCollection extends Construct {
+  /**
+   * The name of the collection.
+   * @private
+   */
+  private collectionName: string;
+
+  /**
+   * The OpenSearch Collection.
+   */
+  public collection: oss.CfnCollection;
+
+  /**
+   * The ARN of the collection.
+   */
+  public collectionArn: string;
+
+  /**
+   * An IAM policy that allows API access to the collection.
+   * @private
+   */
+  private aossPolicy: iam.ManagedPolicy;
+
+  /**
+   * An OpenSearch Access Policy that allows management of the index.
+   */
+  public manageIndexPolicy: oss.CfnAccessPolicy;
+
+  /**
+   * An OpenSearch Access Policy that allows access to the index.
+   */
+  public dataAccessPolicy: oss.CfnAccessPolicy;
+
+  /**
+   * An OpenSearch Access Policy document that will become `dataAccessPolicy`.
+   * @private
+   */
+  private dataAccessPolicyDocument: any[] = [];
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.collectionName = generatePhysicalName('', ['VectorStore'], 32, true, this);
+
+    const encryptionPolicyName = generatePhysicalName('', ['EncryptionPolicy'], 32, true, this);
+    const encryptionPolicy = new oss.CfnSecurityPolicy(this, 'EncryptionPolicy', {
+      name: encryptionPolicyName,
+      type: 'encryption',
+      policy: JSON.stringify({
+        Rules: [
+          {
+            ResourceType: 'collection',
+            Resource: [`collection/${this.collectionName}`],
+          },
+        ],
+        AWSOwnedKey: true,
+      }),
+    });
+
+    const networkPolicyName = generatePhysicalName('', ['NetworkPolicy'], 32, true, this);
+    const networkPolicy = new oss.CfnSecurityPolicy(this, 'NetworkPolicy', {
+      name: networkPolicyName,
+      type: 'network',
+      policy: JSON.stringify([
+        {
+          Rules: [
+            {
+              ResourceType: 'collection',
+              Resource: [`collection/${this.collectionName}`],
+            },
+            {
+              ResourceType: 'dashboard',
+              Resource: [`collection/${this.collectionName}`],
+            },
+          ],
+          AllowFromPublic: true,
+        },
+      ]),
+    });
+
+    this.collection = new oss.CfnCollection(this, 'VectorCollection', {
+      name: this.collectionName,
+      type: 'VECTORSEARCH',
+    });
+
+    this.collectionArn = this.collection.attrArn;
+
+    this.aossPolicy = new iam.ManagedPolicy(
+      this,
+      'AOSSApiAccessAll', {
+        statements: [
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: [
+              'aoss:APIAccessAll',
+            ],
+            resources: [this.collection.attrArn],
+          }),
+        ],
+      },
+    );
+
+    this.collection.addDependency(encryptionPolicy);
+    this.collection.addDependency(networkPolicy);
+
+    const crProvider = AOSSCRProvider.getProvider(this);
+    crProvider.role.addManagedPolicy(this.aossPolicy);
+
+    const manageIndexPolicyName = generatePhysicalName('', ['ManageIndexPolicy'], 32, true, this);
+    this.manageIndexPolicy = new oss.CfnAccessPolicy(this, 'ManageIndexPolicy', {
+      name: manageIndexPolicyName,
+      type: 'data',
+      policy: JSON.stringify([
+        {
+          Rules: [
+            {
+              Resource: [`index/${this.collectionName}/*`],
+              Permission: [
+                'aoss:DescribeIndex',
+                'aoss:CreateIndex',
+                'aoss:DeleteIndex',
+                'aoss:UpdateIndex',
+              ],
+              ResourceType: 'index',
+            },
+            {
+              Resource: [`collection/${this.collectionName}`],
+              Permission: [
+                'aoss:DescribeCollectionItems',
+              ],
+              ResourceType: 'collection',
+            },
+          ],
+          Principal: [
+            crProvider.role.roleArn,
+          ],
+          Description: '',
+        },
+      ]),
+    });
+
+    const isDataAccessPolicyNotEmpty = new cdk.CfnCondition(this, 'IsDataAccessPolicyNotEmpty', {
+      expression: cdk.Fn.conditionNot(cdk.Fn.conditionEquals('[]', cdk.Lazy.string({
+        produce: () => JSON.stringify(this.dataAccessPolicyDocument),
+      }))),
+    });
+
+    const dataAccessPolicyName = generatePhysicalName('', ['DataAccessPolicy'], 32, true, this);
+    this.dataAccessPolicy = new oss.CfnAccessPolicy(this, 'DataAccessPolicy', {
+      name: dataAccessPolicyName,
+      type: 'data',
+      policy: cdk.Lazy.string({
+        produce: () => JSON.stringify(this.dataAccessPolicyDocument),
+      }),
+    });
+    this.dataAccessPolicy.cfnOptions.condition = isDataAccessPolicyNotEmpty;
+  }
+
+  /**
+   * Grants the specified role access to data in the collection.
+   * @param grantee The role to grant access to.
+   */
+  grantDataAccess(grantee: iam.IRole) {
+    this.dataAccessPolicyDocument.push({
+      Rules: [
+        {
+          Resource: [`collection/${this.collectionName}`],
+          Permission: [
+            'aoss:DescribeCollectionItems',
+            'aoss:CreateCollectionItems',
+            'aoss:UpdateCollectionItems',
+          ],
+          ResourceType: 'collection',
+        },
+        {
+          Resource: [`index/${this.collectionName}/*`],
+          Permission: [
+            'aoss:UpdateIndex',
+            'aoss:DescribeIndex',
+            'aoss:ReadDocument',
+            'aoss:WriteDocument',
+            'aoss:CreateIndex',
+          ],
+          ResourceType: 'index',
+        },
+      ],
+      Principal: [
+        grantee.roleArn,
+      ],
+      Description: '',
+    });
+    grantee.addManagedPolicy(this.aossPolicy);
+  }
+}
+
+/**
+ * Metadata field definitions.
+ */
+export interface MetadataManagementField {
+  /**
+   * The name of the field.
+   */
+  MappingField: string;
+  /**
+   * The data type of the field.
+   */
+  DataType: string;
+  /**
+   * Whether the field is filterable.
+   */
+  Filterable: boolean;
+}
+
+/**
+ * Properties for the Custom::OpenSearchIndex custom resource.
+ */
+interface VectorIndexProps {
+  /**
+   * The OpenSearch Endpoint.
+   */
+  Endpoint: string;
+  /**
+   * The name of the index.
+   */
+  IndexName: string;
+  /**
+   * The name of the vector field.
+   */
+  VectorField: string;
+  /**
+   * The number of dimensions in the vector.
+   */
+  Dimensions: number;
+  /**
+   * The metadata management fields.
+   */
+  MetadataManagement: MetadataManagementField[];
+}
+
+/**
+ * Properties for the OpenSearchVectorIndex.
+ */
+export interface OpenSearchVectorIndexProps {
+  /**
+   * The OpenSearch Vector Collection.
+   */
+  collection: OpenSearchVectorCollection;
+  /**
+   * The name of the index.
+   */
+  indexName: string;
+  /**
+   * The name of the vector field.
+   */
+  vectorField: string;
+  /**
+   * The number of dimensions in the vector.
+   */
+  vectorDimensions: number;
+  /**
+   * The metadata management fields.
+   */
+  mappings: MetadataManagementField[];
+}
+
+/**
+ * Deploy a vector index on the collection.
+ */
+export class OpenSearchVectorIndex extends Construct {
+  /**
+   * The vector index resource.
+   */
+  public vectorIndex: cdk.CustomResource;
+
+  constructor(
+    scope: Construct,
+    id: string,
+    props: OpenSearchVectorIndexProps,
+  ) {
+    super(scope, id);
+
+    const crProvider = AOSSCRProvider.getProvider(this);
+
+    this.vectorIndex = new cdk.CustomResource(this, 'VectorIndex', {
+      serviceToken: crProvider.serviceToken,
+      properties: {
+        Endpoint: `${props.collection.collection.attrId}.${cdk.Stack.of(this).region}.aoss.amazonaws.com`,
+        IndexName: props.indexName,
+        VectorField: props.vectorField,
+        Dimensions: props.vectorDimensions,
+        MetadataManagement: props.mappings,
+      } as VectorIndexProps,
+      resourceType: 'Custom::OpenSearchIndex',
+    });
+
+    this.vectorIndex.node.addDependency(props.collection.manageIndexPolicy);
+    this.vectorIndex.node.addDependency(props.collection.collection);
+  }
+
+}
+
+/**
+ * Custom Resource provider for OpenSearch Serverless.
+ *
+ * @internal This is an internal core function and should not be called directly by Solutions Constructs clients.
+ */
+export const AOSSCRProvider = buildCustomResourceProvider({
+  providerName: 'AOSSCRProvider',
+  codePath: path.join(
+    __dirname, '../../../lambda/opensearch-serverless-custom-resources'),
+  handler: 'custom_resources.on_event',
+  runtime: lambda.Runtime.PYTHON_3_12,
+});
---
+++ test/common/helpers/aoss-vectors.test.ts
@@ -0,0 +1,210 @@
+/**
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+
+import * as cdk from 'aws-cdk-lib';
+import { Annotations, Match, Template } from 'aws-cdk-lib/assertions';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { AwsSolutionsChecks, NagSuppressions } from 'cdk-nag';
+import { OpenSearchVectorCollection, OpenSearchVectorIndex } from '../../../src/common/helpers/aoss-vector';
+
+function setupStack() {
+  const app = new cdk.App();
+  cdk.Aspects.of(app).add(new AwsSolutionsChecks());
+  const stack = new cdk.Stack(app, 'test-stack', {
+    env: {
+      account: '123456789012',
+      region: 'us-east-1',
+    },
+  });
+
+  const aossVector = new OpenSearchVectorCollection(stack, 'test-aoss-vector');
+
+  const aossVectorIndex = new OpenSearchVectorIndex(stack, 'test-aoss-vector-index', {
+    collection: aossVector,
+    indexName: 'test-index',
+    vectorField: 'vector',
+    vectorDimensions: 1536,
+    mappings: [
+      {
+        MappingField: 'AMAZON_BEDROCK_TEXT_CHUNK',
+        DataType: 'text',
+        Filterable: true,
+      },
+    ],
+  });
+
+  NagSuppressions.addResourceSuppressionsByPath(
+    stack,
+    '/test-stack/LogRetentionaae0aa3c5b4d4f87b02d85b201efdd8a/ServiceRole',
+    [
+      {
+        id: 'AwsSolutions-IAM4',
+        reason: 'CDK CustomResource LogRetention Lambda uses the AWSLambdaBasicExecutionRole AWS Managed Policy. Managed by CDK.',
+      },
+      {
+        id: 'AwsSolutions-IAM5',
+        reason: 'CDK CustomResource LogRetention Lambda uses a wildcard to manage log streams created at runtime. Managed by CDK.',
+      },
+    ],
+    true,
+  );
+
+  return { app, stack, aossVector, aossVectorIndex };
+}
+
+describe('OpenSearch Serverless Vector Store', () => {
+  describe('No data access policies', () => {
+    let app: cdk.App;
+    let stack: cdk.Stack;
+    let template: Template;
+
+    beforeAll(() => {
+      const s = setupStack();
+      app = s.app;
+      stack = s.stack;
+      app.synth();
+      template = Template.fromStack(stack);
+    });
+
+    test('Should match snapshot', () => {
+      expect(template.toJSON()).toMatchSnapshot();
+    });
+
+    test('Should have the correct resources', () => {
+      template.resourceCountIs('AWS::OpenSearchServerless::Collection', 1);
+      template.resourceCountIs('Custom::OpenSearchIndex', 1);
+      template.resourceCountIs('AWS::OpenSearchServerless::AccessPolicy', 2);
+      template.resourceCountIs('AWS::Lambda::Function', 3);
+    });
+
+    test('DataAccessPolicy should be empty', () => {
+      const conditions = template.findConditions(
+        '*',
+        {
+          'Fn::Not': [
+            {
+              'Fn::Equals': [
+                '[]',
+                '[]',
+              ],
+            },
+          ],
+        },
+      );
+      expect(Object.keys(conditions)).toHaveLength(1);
+      const condition = Object.keys(conditions)[0];
+      template.hasResource(
+        'AWS::OpenSearchServerless::AccessPolicy', {
+          Condition: condition,
+          Properties: {
+            Name: Match.stringLikeRegexp('dataaccesspolicy-[a-z0-9]+'),
+            Policy: '[]',
+            Type: 'data',
+          },
+        });
+    });
+
+    test('No unsuppressed Errors', () => {
+      const errors = Annotations.fromStack(stack).findError(
+        '*',
+        Match.stringLikeRegexp('AwsSolutions-.*'),
+      );
+      expect(errors).toHaveLength(0);
+    });
+  });
+
+
+  describe('With data access policies', () => {
+    let template: Template;
+    let app: cdk.App;
+    let stack: cdk.Stack;
+    let aossVector: OpenSearchVectorCollection;
+    let aossVectorIndex: OpenSearchVectorIndex;
+    let testRole: iam.Role;
+
+    beforeAll(() => {
+      const s = setupStack();
+      app = s.app;
+      stack = s.stack;
+      aossVector = s.aossVector;
+      aossVectorIndex = s.aossVectorIndex;
+
+      aossVectorIndex.node.addDependency(aossVector.dataAccessPolicy);
+
+      testRole = new iam.Role(stack, 'TestRole', {
+        assumedBy: new iam.AccountRootPrincipal(),
+      });
+
+      aossVector.grantDataAccess(testRole);
+
+      app.synth();
+      template = Template.fromStack(stack);
+    });
+
+    test('Should match snapshot', () => {
+      expect(template.toJSON()).toMatchSnapshot();
+    });
+
+    test('Should have the correct resources', () => {
+      template.resourceCountIs('AWS::OpenSearchServerless::Collection', 1);
+      template.resourceCountIs('Custom::OpenSearchIndex', 1);
+      template.resourceCountIs('AWS::OpenSearchServerless::AccessPolicy', 2);
+      template.resourceCountIs('AWS::Lambda::Function', 3);
+    });
+
+    test('DataAccessPolicy should allow testRole', () => {
+      const testRoleLogicalId = stack.getLogicalId(testRole.node.defaultChild as iam.CfnRole);
+
+      const conditions = template.findConditions(
+        '*',
+        {
+          'Fn::Not': [
+            {
+              'Fn::Equals': Match.arrayWith([
+                Match.not('[]'),
+              ]),
+            },
+          ],
+        },
+      );
+      expect(Object.keys(conditions)).toHaveLength(1);
+      const condition = Object.keys(conditions)[0];
+      template.hasResource('AWS::OpenSearchServerless::AccessPolicy', {
+        Condition: condition,
+        Properties: {
+          Name: Match.stringLikeRegexp('dataaccesspolicy-[a-z0-9]+'),
+          Policy: {
+            'Fn::Join': Match.arrayWith([
+              Match.arrayWith([
+                Match.objectEquals({
+                  'Fn::GetAtt': Match.arrayWith([
+                    testRoleLogicalId,
+                  ]),
+                }),
+              ]),
+            ]),
+          },
+          Type: 'data',
+        },
+      });
+    });
+
+    test('No unsuppressed Errors', () => {
+      const errors = Annotations.fromStack(stack).findError(
+        '*',
+        Match.stringLikeRegexp('AwsSolutions-.*'),
+      );
+      expect(errors).toHaveLength(0);
+    });
+  });
+});
