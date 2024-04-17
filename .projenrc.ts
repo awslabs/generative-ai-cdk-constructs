ce8d6953bb949d0295f5224880e66aaa08d73f8c
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2024-04-17 23:08:56 +0000

    fix: update workflows
    Signed-off-by: Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -136,7 +136,8 @@ if (workflowUpgradeMain) {
 }
 
 // Add specific overrides https://projen.io/docs/integrations/github/#actions-versions
-project.github?.actions.set('actions/checkout@v4', 'actions/checkout@f43a0e5ff2bd294095638e18286ca9a3d1956744');
+project.github?.actions.set('actions/checkout@v3', 'actions/checkout@b4ffde65f46336ab88eb53be808477a3936bae11');
+project.github?.actions.set('actions/checkout@v4', 'actions/checkout@b4ffde65f46336ab88eb53be808477a3936bae11');
 project.github?.actions.set('actions/download-artifact@v3', 'actions/download-artifact@9bc31d5ccc31df68ecc42ccf4149144866c47d8a');
 project.github?.actions.set('actions/github-script@v6', 'actions/github-script@d7906e4ad0b1822421a7e6a35d5ca353c962f410');
 project.github?.actions.set('actions/setup-node@v3', 'actions/setup-node@1a4442cacd436585916779262731d5b162bc6ec7');
--- projenrc/github-workflows.ts
+++ projenrc/github-workflows.ts
@@ -232,7 +232,7 @@ export function runSemGrepWorkflow(project: AwsCdkConstructLibrary) {
     name: 'semgrep/ci',
     runsOn: ['ubuntu-latest'],
     container: {
-      image: 'returntocorp/semgrep',
+      image: 'semgrep/semgrep',
     },
     permissions: {
       contents: JobPermission.READ,
@@ -304,7 +304,7 @@ export function runBanditWorkflow(project: AwsCdkConstructLibrary) {
     name: 'bandit/ci',
     runsOn: ['ubuntu-latest'],
     // container: {
-    //   image: 'returntocorp/semgrep',
+    //   image: 'semgrep/semgrep',
     // },
     permissions: {
       contents: JobPermission.READ,
