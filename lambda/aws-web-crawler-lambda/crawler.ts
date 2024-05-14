8452b0483db802147e253652d30369290f36844f
Sergey Pugachev <1313155+spugachev@users.noreply.github.com>
2024-05-14 14:36:33 +0100

    feat(web-crawler): ability to run the crawler as an bedrock agent

--- lambda/aws-web-crawler-lambda/crawler.ts
+++ lambda/aws-web-crawler-lambda/crawler.ts
@@ -1,21 +1,64 @@
+/**
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
 import aws_chromium from '@sparticuz/chromium';
 import { Page } from 'playwright';
 import { JSDOM } from 'jsdom';
 import { Configuration, PlaywrightCrawler, PlaywrightCrawlingContext } from 'crawlee';
-import { CrawlerRequest, PageMetadata } from './types';
+import { CrawlerAgentRequest, CrawlerSimpleRequest, PageMetadata } from './types';
 import { Readability } from '@mozilla/readability';
 
 export class Crawler {
-  async process_crawler_request(request: CrawlerRequest) {
-    const urls = request.urls.map((url) => url.trim());
-    let crawler = await this.create_crawler();
-    await crawler.run(urls);
-    const retValue = await crawler.getData();
+  async process(request: CrawlerSimpleRequest) {
+    const urls = request.urls?.map((url) => url.trim()) ?? [];
 
-    return retValue;
+    let crawler = await this.create_crawler({ include_html: request.include_html });
+    await crawler.run(urls);
+
+    const data = await crawler.getData();
+    const items = data['items'];
+
+    return items;
   }
 
-  async create_crawler() {
+  async process_agent(request: CrawlerAgentRequest) {
+    const urls = request.parameters?.filter((param) => param.name === 'url').map((param) => param.value.trim()) ?? [];
+
+    let crawler = await this.create_crawler({ include_html: false });
+    await crawler.run(urls);
+
+    const data = await crawler.getData();
+    const items = data['items'];
+    const item = items.length > 0 ? items[0] : null;
+
+    return {
+      messageVersion: '1.0',
+      response: {
+        actionGroup: request.actionGroup,
+        apiPath: '/crawler',
+        httpMethod: 'GET',
+        httpStatusCode: 200,
+        responseBody: {
+          'application/json': {
+            body: JSON.stringify(item),
+          },
+        },
+      },
+      sessionAttributes: {},
+      promptSessionAttributes: {},
+    };
+  }
+
+  private async create_crawler(params: { include_html?: boolean }) {
     const crawler = new PlaywrightCrawler(
       {
         launchContext: {
@@ -36,7 +79,7 @@ export class Crawler {
         ],
         requestHandler: async (context: PlaywrightCrawlingContext) => {
           try {
-            await this.requestHandler(context);
+            await this.requestHandler(context, { include_html: params.include_html });
           } catch (e) {
             console.error(e);
           }
@@ -55,7 +98,7 @@ export class Crawler {
     return crawler;
   }
 
-  async requestHandler(ctx: PlaywrightCrawlingContext) {
+  private async requestHandler(ctx: PlaywrightCrawlingContext, params: { include_html?: boolean }) {
     const contentType = await this.getContentType(ctx.response);
     if (contentType !== 'text/html') {
       console.log('Content type', contentType);
@@ -70,25 +113,28 @@ export class Crawler {
 
     const meta = await this.getPageMetadata(ctx.page);
     console.log(`${meta.title}`, { url: ctx.request.loadedUrl });
-    const html_content = await ctx.page.innerHTML('html');
+    const page_html = await ctx.page.innerHTML('html');
 
-    const page = new JSDOM(html_content, {
+    const page = new JSDOM(page_html, {
       url: ctx.request.loadedUrl,
     });
 
     const text_content = new Readability(page.window.document).parse()?.textContent ?? '';
+    const html_content = params.include_html ? page_html : undefined;
 
-    await ctx.pushData({
+    const item = {
       success: true,
       content_type: contentType,
       url: ctx.request.url,
       meta,
-      html_content,
       text_content,
-    });
+      html_content,
+    };
+
+    await ctx.pushData(item);
   }
 
-  async getContentType(response: any | undefined) {
+  private async getContentType(response: any | undefined) {
     const contentType = await response?.headerValue('content-type');
 
     if (contentType) {
@@ -98,7 +144,7 @@ export class Crawler {
     return 'text/html';
   }
 
-  async getPageMetadata(page: Page): Promise<PageMetadata> {
+  private async getPageMetadata(page: Page): Promise<PageMetadata> {
     const title = await page.title();
 
     const getMetaContent = async (tag: string, property = 'name') => {
