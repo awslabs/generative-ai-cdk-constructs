891e1974bb06ad2eb2d0554cf724aae6f73261cf
krokoko <alkrok@amazon.com>
2024-06-28 13:37:05 -0500

    chore(bedrock): add support for claude sonnet 3.5 in qa

--- lambda/aws-qa-appsync-opensearch/question_answering/src/llms/types.py
+++ lambda/aws-qa-appsync-opensearch/question_answering/src/llms/types.py
@@ -14,6 +14,7 @@ class BedrockModel(StrEnum):
     ANTHROPIC_CLAUDE_V2 = 'anthropic.claude-v2',
     ANTHROPIC_CLAUDE_V3_HAIKU_V1 = 'anthropic.claude-3-haiku-20240307-v1:0',
     ANTHROPIC_CLAUDE_V3_SONNET_V1 = 'anthropic.claude-3-sonnet-20240229-v1:0',
+    ANTHROPIC_CLAUDE_V3_5_SONNET_V1 = 'anthropic.claude-3-5-sonnet-20240620-v1:0',
     ANTHROPIC_CLAUDE_INSTANT_V1 = 'anthropic.claude-instant-v1'
     # AMAZON
     AMAZON_TITAN_TEXT_LITE_V1 = 'amazon.titan-text-lite-v1',
@@ -28,6 +29,7 @@ MAX_TOKENS_MAP = {
     Provider.BEDROCK+'.'+BedrockModel.ANTHROPIC_CLAUDE_V2 : 100000,
     Provider.BEDROCK+'.'+BedrockModel.ANTHROPIC_CLAUDE_V3_HAIKU_V1 : 200000,
     Provider.BEDROCK+'.'+BedrockModel.ANTHROPIC_CLAUDE_V3_SONNET_V1 : 200000,
+    Provider.BEDROCK+'.'+BedrockModel.ANTHROPIC_CLAUDE_V3_5_SONNET_V1 : 200000,
     Provider.BEDROCK+'.'+BedrockModel.ANTHROPIC_CLAUDE_INSTANT_V1 : 100000,
     Provider.BEDROCK+'.'+BedrockModel.AMAZON_TITAN_TEXT_LITE_V1 : 4000,
     Provider.BEDROCK+'.'+BedrockModel.AMAZON_TITAN_TEXT_EXPRESS_V1 : 8000,
