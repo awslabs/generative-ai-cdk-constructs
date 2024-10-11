939f10cd7f851fec6781ca94dcefaedb5a6c7202
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2024-10-11 06:57:31 +0000

    feat(test): add snapshot tests

---
+++ test/patterns/gen-ai/aws-llama-index-data-loader/integ-tests/aws-llama-index-data-loader.integ.ts
@@ -0,0 +1,35 @@
+/**
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+import * as integ from '@aws-cdk/integ-tests-alpha';
+import * as cdk from 'aws-cdk-lib';
+import {
+  LlamaIndexDataLoader,
+} from '../../../../../src/patterns/gen-ai/aws-llama-index-data-loader';
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'aws-llama-index-data-loader-integ-test');
+new LlamaIndexDataLoader(stack, 'test', {});
+
+// const integ_case =
+new integ.IntegTest(app, 'ServiceTest', {
+  testCases: [stack],
+  cdkCommandOptions: {
+    destroy: {
+      args: {
+        force: true,
+      },
+    },
+  },
+});
+
+app.synth();
