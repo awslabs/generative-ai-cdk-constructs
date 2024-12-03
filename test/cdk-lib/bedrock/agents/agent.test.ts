6f66067e40dab403b2e26076a9374bffba949850
Rafael Mosca <rafams@amazon.com>
2024-12-03 16:16:01 +0100

    refactor(bedrock): agents refactor - add tests part 2

--- test/cdk-lib/bedrock/agents/agent.test.ts
+++ test/cdk-lib/bedrock/agents/agent.test.ts
@@ -43,7 +43,7 @@ describe('CDK-Agent', () => {
       });
     });
 
-    test('Core Properties', () => {
+    test('Core Properties - Defaults', () => {
       const agent = new bedrock.Agent(stack, 'TestAgent', {
         name: 'TestAgent',
         description: 'This is a test agent',
@@ -62,6 +62,7 @@ describe('CDK-Agent', () => {
       // Default Properties
       expect(agent.idleSessionTTL.toHours()).toBe(1);
       expect(agent.forceDelete).toBe(false);
+      expect(agent.shouldPrepareAgent).toBe(false);
       expect(agent.lastUpdated).toBeDefined();
       expect(agent.agentArn).toBeDefined();
       expect(agent.agentId).toBeDefined();
@@ -75,6 +76,31 @@ describe('CDK-Agent', () => {
       expect(agent.kmsKey).toBeUndefined();
     });
 
+    test('Core Properties - Custom', () => {
+      const agent = new bedrock.Agent(stack, 'TestAgent', {
+        name: 'TestAgent',
+        description: 'This is a test agent',
+        instruction: 'This is a test instruction',
+        foundationModel: bedrock.BedrockFoundationModel.ANTHROPIC_CLAUDE_3_5_SONNET_V1_0,
+        // Custom below
+        idleSessionTTL: cdk.Duration.hours(2),
+        forceDelete: true,
+        shouldPrepareAgent: true,
+      });
+
+      Template.fromStack(stack).hasResourceProperties('AWS::Bedrock::Agent', {
+        AgentName: 'TestAgent',
+        Description: 'This is a test agent',
+        AutoPrepare: true,
+        IdleSessionTTLInSeconds: 7200,
+        SkipResourceInUseCheckOnDelete: true,
+      });
+
+      expect(agent.idleSessionTTL.toHours()).toBe(2);
+      expect(agent.forceDelete).toBe(true);
+      expect(agent.shouldPrepareAgent).toBe(true);
+    });
+
     test('Core Creation + Custom KMS Key', () => {
       const agent = new bedrock.Agent(stack, 'TestAgent', {
         name: 'TestAgent',
@@ -84,7 +110,7 @@ describe('CDK-Agent', () => {
         kmsKey: Key.fromKeyArn(
           stack,
           'importedKey',
-          'arn:aws:kms:eu-central-1:123456789012:key/06484191-7d55-49fb-9be7-0baaf7fe8418',
+          'arn:aws:kms:eu-central-1:123456789012:key/06484191-7d55-49fb-9be7-0baaf7fe8418'
         ),
       });
 
@@ -95,7 +121,7 @@ describe('CDK-Agent', () => {
       });
 
       expect(agent.kmsKey!.keyArn).toBe(
-        'arn:aws:kms:eu-central-1:123456789012:key/06484191-7d55-49fb-9be7-0baaf7fe8418',
+        'arn:aws:kms:eu-central-1:123456789012:key/06484191-7d55-49fb-9be7-0baaf7fe8418'
       );
     });
 
@@ -103,7 +129,7 @@ describe('CDK-Agent', () => {
       const myRole = Role.fromRoleArn(
         stack,
         'myRole',
-        'arn:aws:iam::123456789012:role/AmazonBedrockExecutionRoleForAgent_12345678',
+        'arn:aws:iam::123456789012:role/AmazonBedrockExecutionRoleForAgent_12345678'
       );
       const agent = new bedrock.Agent(stack, 'TestAgent', {
         name: 'TestAgent',
@@ -328,7 +354,7 @@ describe('CDK-Agent', () => {
 
       // THEN
       expect(() => agent.addGuardrail(anotherGuardrail)).toThrow(
-        'Cannot add Guardrail yib23y5g23b2wf. Guardrail oygh3o8g7rtl has already been specified for this agent.',
+        'Cannot add Guardrail yib23y5g23b2wf. Guardrail oygh3o8g7rtl has already been specified for this agent.'
       );
     });
   });
@@ -402,7 +428,7 @@ describe('CDK-Agent', () => {
         {
           knowledgeBaseId: 'ABCDEFG1234',
           executionRoleArn: 'arn:aws:iam::123456789012:role/AmazonBedrockExecutionRoleForKnowledgeBase_12345678',
-        },
+        }
       ));
 
       const agent = new bedrock.Agent(stack, 'TestAgent', {
@@ -412,8 +438,13 @@ describe('CDK-Agent', () => {
       });
 
       expect(() => agent.addKnowledgeBase(invalidKb)).toThrow(
-        'If instructionForAgents is not provided, the description property of the KnowledgeBase ABCDEFG1234 must be provided.',
+        'If instructionForAgents is not provided, the description property of the KnowledgeBase ABCDEFG1234 must be provided.'
       );
     });
   });
+
+  /*******************************************************
+   *              PROMPT OVERRIDE in Agents				 *
+   *******************************************************/
+  describe('Prompt Override', () => {});
 });
