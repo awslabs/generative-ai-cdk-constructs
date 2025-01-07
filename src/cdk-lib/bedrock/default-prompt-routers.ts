e2f2bdd9e67c85b7d3b8baa361ed4a5273f36c89
Rafael Mosca <rafams@amazon.com>
2025-01-07 13:30:35 +0100

    fix(bedrock): add changes

--- src/cdk-lib/bedrock/default-prompt-routers.ts
+++ src/cdk-lib/bedrock/default-prompt-routers.ts
@@ -24,51 +24,59 @@ export interface PromptRouterProps {
    */
   readonly promptRouterId: string;
   /**
-   * The foundation models / profiles this router will route to.
+   * The foundation models this router will route to.
    */
-  readonly routingEndpoints: IInvokable[];
+  readonly routingModels: BedrockFoundationModel[];
+}
+
+/**
+ * Represents identifiers for default prompt routers in Bedrock
+ */
+export class DefaultPromptRouterIdentifier {
+  /**
+   * Anthropic Claude V1 router configuration
+   */
+  public static readonly ANTHROPIC_CLAUDE_V1 = new DefaultPromptRouterIdentifier({
+    promptRouterId: 'anthropic.claude:1',
+    routingModels: [
+      BedrockFoundationModel.ANTHROPIC_CLAUDE_HAIKU_V1_0,
+      BedrockFoundationModel.ANTHROPIC_CLAUDE_3_5_SONNET_V1_0,
+    ],
+  });
+
+  /**
+   * Meta Llama 3.1 router configuration
+   */
+  public static readonly META_LLAMA_3_1 = new DefaultPromptRouterIdentifier({
+    promptRouterId: 'meta.llama:1',
+    routingModels: [
+      BedrockFoundationModel.META_LLAMA_3_1_8B_INSTRUCT_V1,
+      BedrockFoundationModel.META_LLAMA_3_1_70B_INSTRUCT_V1,
+    ],
+  });
+
+  public readonly promptRouterId: string;
+  public readonly routingModels: BedrockFoundationModel[];
+  private constructor(props: PromptRouterProps) {
+    (this.promptRouterId = props.promptRouterId), (this.routingModels = props.routingModels);
+  }
 }
 
 export class PromptRouter implements IInvokable, IPromptRouter {
-  public static readonly ANTHROPIC_CLAUDE_V1 = new PromptRouter({
-    promptRouterId: 'anthropic.claude:1',
-    routingEndpoints: [
-      CrossRegionInferenceProfile.fromConfig({
-        model: BedrockFoundationModel.ANTHROPIC_CLAUDE_HAIKU_V1_0,
-        geoRegion: REGION_TO_GEO_AREA[Aws.REGION],
-      }),
-      CrossRegionInferenceProfile.fromConfig({
-        model: BedrockFoundationModel.ANTHROPIC_CLAUDE_3_5_SONNET_V1_0,
-        geoRegion: REGION_TO_GEO_AREA[Aws.REGION],
-      }),
-    ],
-  });
-
-  public static readonly META_LLAMA_3_1 = new PromptRouter({
-    promptRouterId: 'meta.llama:1',
-    routingEndpoints: [
-      CrossRegionInferenceProfile.fromConfig({
-        model: BedrockFoundationModel.META_LLAMA_3_1_8B_INSTRUCT_V1,
-        geoRegion: REGION_TO_GEO_AREA[Aws.REGION],
-      }),
-      CrossRegionInferenceProfile.fromConfig({
-        model: BedrockFoundationModel.META_LLAMA_3_1_70B_INSTRUCT_V1,
-        geoRegion: REGION_TO_GEO_AREA[Aws.REGION],
-      }),
-    ],
-  });
-
+  public static fromDefault(defaultRouter: DefaultPromptRouterIdentifier, region: string) {
+    return new PromptRouter(defaultRouter, region);
+  }
   public readonly promptRouterArn: string;
   public readonly promptRouterId: string;
   public readonly invokableArn: string;
   public readonly routingEndpoints: IInvokable[];
 
-  constructor(props: PromptRouterProps) {
+  constructor(props: PromptRouterProps, region: string) {
     this.promptRouterId = props.promptRouterId;
     this.promptRouterArn = Arn.format({
       partition: Aws.PARTITION,
       service: 'bedrock',
-      region: Aws.REGION,
+      region: region,
       account: Aws.ACCOUNT_ID,
       resource: 'default-prompt-router',
       resourceName: this.promptRouterId,
@@ -76,7 +84,14 @@ export class PromptRouter implements IInvokable, IPromptRouter {
     });
     // needed to implement IInvokable
     this.invokableArn = this.promptRouterArn;
-    this.routingEndpoints = props.routingEndpoints;
+
+    // build inference profiles from routing endpoints
+    this.routingEndpoints = props.routingModels.flatMap(model => {
+      return CrossRegionInferenceProfile.fromConfig({
+        model: model,
+        geoRegion: REGION_TO_GEO_AREA[region],
+      });
+    });
   }
 
   grantInvoke(grantee: IGrantable): Grant {
@@ -87,7 +102,7 @@ export class PromptRouter implements IInvokable, IPromptRouter {
     // Grant invoke to the prompt router
     return Grant.addToPrincipal({
       grantee,
-      actions: ['bedrock:InvokeModel'],
+      actions: ['bedrock:GetPromptRouter', 'bedrock:InvokeModel'],
       resourceArns: [this.promptRouterArn],
     });
   }
