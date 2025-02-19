0b36faeadf61c5c553a1418845c1a005f8e6cca9
dinsajwa <dinsajwa@amazon.com>
2025-02-19 11:40:33 -0500

    feat(construct): merged with main

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,5 @@ export * from './common/base-class/base-class';
 export * from './common/base-class/construct-name-enum';
 export * from './patterns/gen-ai/aws-bedrock-cw-dashboard';
 export * from './patterns/gen-ai/aws-aoss-cw-dashboard';
-export * from './patterns/gen-ai/aws-llama-index-data-loader';
 export * from './patterns/gen-ai/aws-bedrock-data-automation';
 export * from './patterns/gen-ai/aws-bedrock-batch-stepfn';
